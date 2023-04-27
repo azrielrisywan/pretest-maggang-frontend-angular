837b619d385f002ea1b60455336391aa15ee5d34
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-04-27 20:16:05 +0700

    update feature checkout on cart

--- test-frontend-angular/src/app/app.module.ts
+++ test-frontend-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,10 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { ItemComponent } from './item/item.component';
 import { TransactionComponent } from './transaction/transaction.component';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 const appRoutes: Routes = [
   { path: 'catalog', component: CatalogComponent },
@@ -27,6 +32,10 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    ToastrModule.forRoot(),
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test-frontend-angular/src/app/cart/cart.component.ts
+++ test-frontend-angular/src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+
+  constructor(private toastr: ToastrService, private http: HttpClient) { }
+
+  onCheckout(form: NgForm) {
+    console.log("Checkout");
+    const postData = {
+      items: ["Bola", "Dumbbell"]
+    }
+    this.http.post('https://test-frontend-angular-68cde-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData).subscribe(response => {
+      console.log(response);
+    })
+    this.toastr.success('Form submitted successfully');
+    form.resetForm
+  }
 
 }
